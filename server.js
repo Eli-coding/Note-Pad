8b694f89dc89e71249cef76c6ad088ad66b0b073
Elizabeth Rodriguez <elizabethyoannie@outlook.com>
2022-03-03 21:13:19 -0400

    details

--- server.js
+++ server.js
@@ -6,12 +6,15 @@ const { v4: uuidv4 } = require('uuid');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }))
 
 app.use(express.static("public"))
+
+//routes
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
